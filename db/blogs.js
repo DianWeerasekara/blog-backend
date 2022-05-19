a31f3951af9cf2a922bca1513f03c5e2c6ab2dae
DianWeerasekara <77507638+DianWeerasekara@users.noreply.github.com>
2022-05-19 23:05:20 +0530

    mock data

---
+++ db/blogs.js
@@ -0,0 +1,14 @@
+var blogs = [
+    {
+        id: 1,
+        description: "This is the first blog of the system",
+        author: "Yohan Amarathunga"
+    },
+    {
+        id: 2,
+        description: "This is the second blog",
+        author: "Redington"
+    }
+];
+
+module.exports = blogs;

192a9b1274e2e4365c05b9f720722ce9ebc1344c
DianWeerasekara <77507638+DianWeerasekara@users.noreply.github.com>
2022-05-21 13:56:42 +0530

    test case finalized

--- index.test.js
+++ index.test.js
@@ -1,6 +1,9 @@
 const app = require("./index");
 const request = require('supertest');
 
+const blogId = 2;
+const wId = 574;
+
 describe("GET /blogs", () => {
     test("shoud respond with a 200 status code", async () => {
         const response = await request(app).get("/blogs")
@@ -45,13 +48,47 @@ describe("POST /blogs", () => {
 
 describe("GET /blogs/:id", () => {
     test("shoud respond with a 200 status code", async () => {
-        const response = await request(app).get("/blogs/:id")
+        const response = await request(app).get("/blogs/"+ blogId)
         expect(response.statusCode).toBe(200)
     })
     test("respond with json content type", async () => {
-        const response = await request(app).get("/blogs/:id")
+        const response = await request(app).get("/blogs/"+ blogId)
         expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
     })
-    
+    test("response has data", async () => {
+        const response = await request(app).get("/blogs/"+blogId)
+        expect(response.body).toBeDefined()
+    })
+    test("shoud respond with a 404 status code", async () => {
+        const response = await request(app).get("/blogs/"+wId)
+        expect(response.statusCode).toBe(404)
+    })
 });
 
+
+describe("PUT /blogs/:id", () => {
+    describe("When blog details provided", () => {
+        test("shoud respond with a 200 status code", async () => {
+            const response = await request(app).put("/blogs/"+ blogId).send({
+                description: "This is a new blog",
+                author: "Dian Weerasekara"
+            })
+            expect(response.statusCode).toBeTruthy()
+        })
+        test("respond with json content type", async () => {
+            const response = await request(app).put("/blogs/"+ blogId).send({
+                description: "This is a new blog",
+                author: "Dian Weerasekara"
+            })
+            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+        })
+        test("when provided item is not found should respond with 404", async () => {
+            const response = await request(app).put("/blogs/"+wId).send({
+                description: "This is a new blog",
+                author: "Dian Weerasekara"
+            })
+            expect(response.statusCode).toBe(404)
+        })
+    })
+
+});
